63229ccd08809b0674264ef3c4e044ace8ad1138
Michal Kristofik <m.kristofik@yahoo.com>
2021-12-19 22:57:48 +0100

    express.urlencoded

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 // Config the app, include middlewares
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // Config DB connection
 const mongoose = require("mongoose");

6ea34749a4f92aadcf2db34752915db75a7beb68
Michal Kristofik <m.kristofik@yahoo.com>
2021-12-19 19:53:45 +0100

    dotenv as development dependency

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Load module dependencies
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 
